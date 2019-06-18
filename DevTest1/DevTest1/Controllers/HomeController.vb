304960ab0183ee240b4ce0d773d307064523aa42
Pravin Nerkar <pravin.nerkar@gmail.com>
2019-06-18 17:01:03 -0500

    Performance issue was because of excessive string manupilation  .  String Builde has beeter performance for string manupilation

--- DevTest1/DevTest1/Controllers/HomeController.vb
+++ DevTest1/DevTest1/Controllers/HomeController.vb
@@ -13,15 +13,26 @@
 
         Dim csvHeader As String = "Company, Name, EmailAddress" & vbCr
         Dim csvBody As String = ""
+        Dim csvBodyStringBuilder As StringBuilder = New StringBuilder()
+        ''build body
+        'For Each c As Customer In list
+        '    csvBody &= c.Company & ", "
+        '    csvBody &= c.Name & ", "
+        '    csvBody &= c.EmailAddress & vbCr
+        'Next
 
         'build body
         For Each c As Customer In list
-            csvBody &= c.Company & ", "
-            csvBody &= c.Name & ", "
-            csvBody &= c.EmailAddress & vbCr
+            csvBodyStringBuilder.Append(c.Company)
+            csvBodyStringBuilder.Append(",")
+            csvBodyStringBuilder.Append(c.Name)
+            csvBodyStringBuilder.Append(",")
+            csvBodyStringBuilder.Append(c.EmailAddress)
+            csvBodyStringBuilder.Append(vbCr)
         Next
 
-        ViewBag.CsvReport = csvHeader & csvBody
+        'ViewBag.CsvReport = csvHeader & csvBody
+        ViewBag.CsvReport = csvHeader & csvBodyStringBuilder.ToString()
 
         Return View()
 
